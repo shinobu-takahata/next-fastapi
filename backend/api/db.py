e2493b277f08486df10b1718edee7b1602cc5624
shinobu-takahata <shinoshino2335@gmail.com>
2024-01-23 15:20:25 +0900

    backend　Lambda用に修正

--- backend/api/db.py
+++ backend/api/db.py
@@ -1,7 +1,19 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+load_dotenv()
 
-ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
+# 環境変数から値を取得
+DB_USER = os.getenv('DB_USER', 'root')
+DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+HOST_NAME = os.getenv('HOST_NAME', 'localhost')
+DB_NAME = os.getenv('DB_NAME', 'demo')
+DB_PORT = os.getenv('DB_PORT', '3306')
+
+# 接続文字列を構築
+ASYNC_DB_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{HOST_NAME}:{DB_PORT}/{DB_NAME}?charset=utf8"
+# ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
 
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
 async_session = sessionmaker(
--- backend/api/main.py
+++ backend/api/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from api.routers import task, done
+from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
 
 app = FastAPI()
+# CORSを設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Next.jsアプリのURL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(task.router)
 app.include_router(done.router)
+
+handler = Mangum(app)
